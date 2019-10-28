4281f706b306300fee2e34a38f91e2c5bc1b2b47
Robert Stirling <rwstirling@uh.edu>
2019-10-28 08:50:56 -0500

    changed relationships but still cant delete any physicians

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -1,5 +1,5 @@
 class Physician < ApplicationRecord
-    has_many :appointments
+    has_many :patients, through: :appointments
     has_many :offices, inverse_of: :physician
     accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy: true
 
